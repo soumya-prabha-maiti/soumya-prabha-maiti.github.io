f8b4519bf8735d4533e009cd4454ef7a77eff11e
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2023-09-04 22:09:36 +0530

    Add social media icons to hero section

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
-import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, HeroSocialMediaIcons, HeroSocialMediaIcon } from './HeroStyle'
 import HeroImg from '../../images/HeroImage.jpg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const HeroSection = () => {
     return (
@@ -14,7 +17,7 @@ const HeroSection = () => {
                 </HeroBg>
                 <HeroInnerContainer >
                     <HeroLeftContainer id="Left">
-                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <Title>{Bio.name}</Title>
                         {/* <TextLoop>
                             I am a
                             <Span>
@@ -28,7 +31,13 @@ const HeroSection = () => {
                             </Span>
                         </TextLoop> */}
                         <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='_blank'>Check Resume</ResumeButton>
+                        
+                        <HeroSocialMediaIcons>
+                            <HeroSocialMediaIcon href={Bio.linkedin} target="_blank"><LinkedInIcon /></HeroSocialMediaIcon>
+                            <HeroSocialMediaIcon href={Bio.github} target="_blank"><GitHubIcon /></HeroSocialMediaIcon>
+                            <HeroSocialMediaIcon href={Bio.email} target="_blank"><EmailIcon /></HeroSocialMediaIcon>
+                        </HeroSocialMediaIcons>
+
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
@@ -36,6 +45,7 @@ const HeroSection = () => {
                         <Img src={HeroImg} alt="hero-image" />
                     </HeroRightContainer>
                 </HeroInnerContainer>
+
 
             </HeroContainer>
         </div>
