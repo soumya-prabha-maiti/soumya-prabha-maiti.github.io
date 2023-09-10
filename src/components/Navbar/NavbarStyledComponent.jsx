d410f5173bd036c127a0b60eea7c031a7c8cdc7e
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2023-09-10 22:39:34 +0530

    Fix: Mobile menu remained open
    Make open mobile menu disappear when screen crosses 768px

--- src/components/Navbar/NavbarStyledComponent.jsx
+++ src/components/Navbar/NavbarStyledComponent.jsx
@@ -109,23 +109,25 @@ export const MobileIcons = styled.div`
 `
 
 export const MobileMenu = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 16px;
-    position: absolute;
-    top: 80px;
-    right: 0;
-    width: 100%;
-    padding: 12px 40px 24px 40px;
-    background: ${({ theme }) => theme.card_light};
-    transition: all 0.6s ease-in-out;
-    transform: ${({ isOpen }) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
-    border-radius: 0 0 20px 20px;
-    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
-    opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
-    z-index: ${({ isOpen }) => (isOpen ? '1000' : '-1000')};
-
+    display: none;
+    @media screen and (max-width: 768px){
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 16px;
+      position: absolute;
+      top: 80px;
+      right: 0;
+      width: 100%;
+      padding: 12px 40px 24px 40px;
+      background: ${({ theme }) => theme.card_light};
+      transition: all 0.6s ease-in-out;
+      transform: ${({ isOpen }) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
+      border-radius: 0 0 20px 20px;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+      opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+      z-index: ${({ isOpen }) => (isOpen ? '1000' : '-1000')};
+    }
 `
 
 export const MobileMenuItems = styled.ul`
