2287aa7dda9acb9df786ed80f77f306f755e321b
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2023-09-19 19:00:05 +0530

    Update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -90,10 +90,10 @@ function Footer() {
         <Logo>Soumya Prabha Maiti</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
-          <NavLink href="#skills">Skills</NavLink>
-          <NavLink href="#experience">Experience</NavLink>
-          <NavLink href="#projects">Projects</NavLink>
           <NavLink href="#education">Education</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
         </Nav>
         <SocialMediaIcons>
           <SocialMediaIcon href={Bio.linkedin} target="_blank"><LinkedInIcon /></SocialMediaIcon>
