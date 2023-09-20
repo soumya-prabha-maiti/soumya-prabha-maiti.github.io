02c7edd5fbc502e5d83af64bdc53c9c9ac524d01
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2023-09-21 01:24:00 +0530

    Set each link to open in its own new tab

--- src/components/ProjectDetails/index.jsx
+++ src/components/ProjectDetails/index.jsx
@@ -214,10 +214,10 @@ const index = ({ openModal, setOpenModal }) => {
                                     <Member>
                                         <MemberImage src={member.img} />
                                         <MemberName>{member.name}</MemberName>
-                                        <a href={member.github} target="new" style={{textDecoration: 'none', color: 'inherit'}}>
+                                        <a href={member.github} target="_blank" style={{textDecoration: 'none', color: 'inherit'}}>
                                             <GitHub />
                                         </a>
-                                        <a href={member.linkedin} target="new" style={{textDecoration: 'none', color: 'inherit'}}>
+                                        <a href={member.linkedin} target="_blank" style={{textDecoration: 'none', color: 'inherit'}}>
                                             <LinkedIn />
                                         </a>
                                     </Member>
@@ -226,8 +226,8 @@ const index = ({ openModal, setOpenModal }) => {
                         </>
                     )}
                     <ButtonGroup>
-                        <Button dull href={project?.github} target='new'>View Code</Button>
-                        <Button href={project?.webapp} target='new'>View Live App</Button>
+                        <Button dull href={project?.github} target='_blank'>View Code</Button>
+                        <Button href={project?.webapp} target='_blank'>View Live App</Button>
                     </ButtonGroup>
                 </Wrapper>
             </Container>
